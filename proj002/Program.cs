d60d9cf70f9632abd93864e66d117a1857a8bf7d
Oleg.Stelm <oleg.stelm@gmail.com>
2022-12-15 00:15:35 +0300

    EditProj002

--- proj002/Program.cs
+++ proj002/Program.cs
@@ -3,14 +3,14 @@
 [3, 7, 23, 12] -> 19
 [-4, -6, 89, 6] -> 0    */
 
-int[] array = new int[5];
+int[] array = new int[6];
 int sum = 0;
 Console.Write($"[ ");
 for (int i = 0; i < array.Length; i++)
 {
     array[i] = new Random().Next(-99, 100);
     Console.Write($"{array[i]} ");
-    if (i % 2 == 0)
+    if (i % 2 != 0)
     {
         sum += array[i];
     }
