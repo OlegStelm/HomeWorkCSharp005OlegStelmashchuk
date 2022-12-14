3f705703da96d2c2c8d9e983ab7788c5bd0d6ecf
Oleg.Stelm <oleg.stelm@gmail.com>
2022-12-15 00:56:53 +0300

    AddProj003

---
+++ proj003/Program.cs
@@ -0,0 +1,27 @@
+﻿/*  **Задача 38**
+Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+[3 7 22 2 78] -> 76 */
+
+double[] array = new double[6];
+double diff = 0;
+double min = array[0];
+double max = array[0];
+Console.Write($"[ ");
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(-100,100) / 10.0;
+    Console.Write($"{array[i]} ");
+    if (array[i] > max)
+    {
+        max = array[i];
+    }
+    else
+        if (array[i] < min)
+        {
+        min = array[i];
+        }
+}
+diff = max - min;
+Console.WriteLine($"]");
+Console.Write($"Difference between minimum and maximum elements = {diff}");
