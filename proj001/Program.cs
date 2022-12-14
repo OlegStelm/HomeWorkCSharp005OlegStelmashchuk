8e9b8b41569634a4fd1c76f31942bf137a76d2c4
Oleg.Stelm <oleg.stelm@gmail.com>
2022-12-14 23:55:17 +0300

    InitCommit

---
+++ proj001/Program.cs
@@ -0,0 +1,25 @@
+﻿/*  **Задача 34**
+Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+[345, 897, 568, 234] -> 2   */
+
+int[] array = new int[20];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(100, 1000);
+}
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] % 2 == 0)
+    {
+        count++;
+    }
+}
+Console.Write($"[ ");
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write($"{array[i]} ");
+}
+Console.WriteLine($"]");
+Console.Write($"The number of even elements in the array count - {count}");
