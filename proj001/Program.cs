8d4dca1cfb44d951bad6273b0fc1255840b21d8a
Oleg.Stelm <oleg.stelm@gmail.com>
2022-12-15 00:08:51 +0300

    EditProj001

--- proj001/Program.cs
+++ proj001/Program.cs
@@ -4,19 +4,16 @@
 [345, 897, 568, 234] -> 2   */
 
 int[] array = new int[20];
+int count = 0;
 Console.Write($"[ ");
 for (int i = 0; i < array.Length; i++)
 {
     array[i] = new Random().Next(100, 1000);
     Console.Write($"{array[i]} ");
-}
-Console.WriteLine($"]");
-int count = 0;
-for (int i = 0; i < array.Length; i++)
-{
     if (array[i] % 2 == 0)
     {
         count++;
     }
 }
+Console.WriteLine($"]");
 Console.Write($"The number of even elements in the array count - {count}");
